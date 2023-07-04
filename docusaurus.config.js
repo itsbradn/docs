41e81825a95b3724670bc9862e358c4896be7033
braden <imbradn@protonmail.com>
2023-07-03 18:46:35 -0700

    Build fail fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,20 +8,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'bradn docs',
+  tagline: 'Documentation for all bradn branded projects.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.bradn.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'itsbradn', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import { API } from '@stoplight/elements';
-import '@stoplight/elements/styles.min.css';
-import useBreakpoint from '../lib/useBreakPoint';
+import React from "react";
+import { API } from "@stoplight/elements";
+import "@stoplight/elements/styles.min.css";
+import useBreakpoint from "../lib/useBreakPoint";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  
   const size = useBreakpoint();
   return (
     <>
-    <header className="header">
-      <a href="/" className="header__logo"><h1>bradn</h1></a>
-    </header>
+      <header className="header">
+        <a href="/" className="header__logo">
+          <h1>bradn</h1>
+        </a>
+      </header>
       <main>
-        <API
-          layout={size === 'sm' ? 'stacked' : 'sidebar'}
-          apiDescriptionUrl="/api/v1.yaml"
-          router="hash"
-        />
+        <BrowserOnly>
+          {() => (
+            <API
+              layout={size === "sm" ? "stacked" : "sidebar"}
+              apiDescriptionUrl="/api/v1.yaml"
+              router="hash"
+            />
+          )}
+        </BrowserOnly>
       </main>
-      </>
+    </>
   );
 }
