f9c3932009b5044f558e93540ddd5f161612e6cb
braden <imbradn@protonmail.com>
2023-07-03 19:08:02 -0700

    Fix theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,8 +63,14 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         title: 'My Site',
         logo: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,9 @@ export default function Home(): JSX.Element {
             />
           )}
         </BrowserOnly>
+        <BrowserOnly>
+          {() => <script>document.documentElement.setAttribute('data-theme', 'dark')</script>}
+        </BrowserOnly>
       </main>
     </>
   );
